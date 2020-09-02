68167c0115b2d4d1a89a20e322c1e5e081fef550
grant <ligaolong@aliyun.com>
2020-09-02 17:55:56 +0800

    [version]:1.0.0 [修改内容]: [新增功能]:创建网关应用gateway [Note]:

---
+++ change-gateway/src/main/java/com/hilamg/change/ZuulGatewayApplication.java
@@ -0,0 +1,19 @@
+package com.hilamg.change;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * 网关入口
+ * @EnableZuulProxy 标识网关服务器
+ * @SpringCloudApplication 组合@SpringBootApplication注解
+ */
+@EnableZuulProxy
+@SpringCloudApplication
+public class ZuulGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulGatewayApplication.class,args);
+    }
+}
