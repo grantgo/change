958b53d1b391fea8e0fb17da91213bb98f1821be
grant <ligaolong@aliyun.com>
2020-09-02 18:25:23 +0800

    [version]:1.0.0 [修改内容]: [新增功能]:自定义过滤器(未完成) [Note]:

---
+++ change-gateway/src/main/java/com/hilamg/change/filter/AbstractZuulFilter.java
@@ -0,0 +1,31 @@
+package com.hilamg.change.filter;
+
+import com.netflix.zuul.ZuulFilter;
+import com.netflix.zuul.context.RequestContext;
+import com.netflix.zuul.exception.ZuulException;
+
+/**
+ * 通用抽象过滤器类
+ */
+public class AbstractZuulFilter extends ZuulFilter {
+
+    @Override
+    public String filterType() {
+        return null;
+    }
+
+    @Override
+    public int filterOrder() {
+        return 0;
+    }
+
+    @Override
+    public boolean shouldFilter() {
+        return false;
+    }
+
+    @Override
+    public Object run() throws ZuulException {
+        return null;
+    }
+}
